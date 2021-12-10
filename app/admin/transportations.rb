253999cc150f8750755d6ab17b958c939282f560
anonymous anonymous <ian.chou@kellogg.northwestern.edu>
2021-12-10 04:22:15 +0000

    Whitelist columns in Transportation for ActiveAdmin

--- app/admin/transportations.rb
+++ app/admin/transportations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Transportation do
 
+ permit_params :method, :speed
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
