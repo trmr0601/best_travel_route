0857204d0f5b7e86a8796e43eaf37844a27ed548
anonymous anonymous <ian.chou@kellogg.northwestern.edu>
2021-12-10 04:22:38 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,13 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :age, :string
+  attribute :travel_preference, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
