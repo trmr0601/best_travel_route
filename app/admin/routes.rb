3e354c81f3c2d782e0031a576b36cc0e511beae4
anonymous anonymous <ian.chou@kellogg.northwestern.edu>
2021-12-10 04:22:09 +0000

    Whitelist columns in Route for ActiveAdmin

--- app/admin/routes.rb
+++ app/admin/routes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Route do
 
+ permit_params :start_point, :end_point, :distance, :transportations_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
